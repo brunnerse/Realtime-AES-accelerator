7cfa1e695b274e54e0f90ae7277b78f56810e236
Severin Brunner <severin.brunner@outlook.de>
2022-08-04 17:27:59 +0200

    Implement MixColumns encryption

--- AES/AES.srcs/sources_1/new/MixColumns.vhd
+++ AES/AES.srcs/sources_1/new/MixColumns.vhd
@@ -25,12 +25,8 @@ use work.common.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity MixColumns is
     Port ( dIn : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
@@ -44,16 +40,72 @@ end MixColumns;
 
 architecture Behavioral of MixColumns is
 
+component VectorToColumnTable Port
+    ( vector : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
+           table  : out COLUMNTABLE
+    );
+end component;
+component ColumnTableToVector is
+    Port (           
+        table  : in COLUMNTABLE;
+        vector : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0)
+    );
+end component;
+
+
+function multiplyBy2(val : std_logic_vector(7 downto 0)) return std_logic_vector is
+    variable c : std_logic_vector(7 downto 0);
+begin
+    c := val(6 downto 0) & '0';
+    if val >= x"80" then
+        return c xor x"1b";
+    else
+        return c;
+    end if;
+end function;
+
+signal tableIn, tableOut : COLUMNTABLE;
+
 begin
 
+dinToTable:   VectorToColumnTable port map(din, tableIn);
+tableToDout : ColumnTableToVector port map(tableOut, dout);
+
+
 process (Clock, Reset)
+
+variable times2, times3, a3 : std_logic_vector(7 downto 0);
+variable extendedCol : std_logic_vector(55 downto 0);
+variable extColLine : integer;
+
 begin
 if Reset = '0' then
     dout <= (others => '0');
 elsif rising_edge(Clock) then
     EnO <= EnI;
     if EnI = '1' then
+        if encrypt = '1' then
+            for col in KEY_SIZE/32-1 downto 0 loop
+                -- Extend the column by its first three elements for simpler indexing
+                extendedCol := tableIn(col)(31 downto 0) & tableIn(col)(31 downto 8);
 
+                for line in 3 downto 0 loop
+                    extColLine := line + 3;
+                    -- Multiply the byte in the current cell [line, col] by 2
+                    times2 := multiplyBy2(extendedCol(extColLine*8+7 downto extColLine*8));
+                    -- Multiply the byte in the next cell by 2
+                    a3 := extendedCol(extColLine*8-1 downto extColLine*8-8);
+                    times3 := multiplyBy2(a3) xor a3;
+                    -- xor all the results
+                    tableOut(col)(line*8+7 downto line*8) <= times2 xor times3
+                        xor extendedCol(extColLine*8-9 downto extColLine*8-16)
+                        xor extendedCol(extColLine*8-17 downto extColLine*8-24);
+                end loop;               
+            end loop;
+        else
+            -- decrypt
+            
+        end if;
     end if;
 end if;
 end process;
