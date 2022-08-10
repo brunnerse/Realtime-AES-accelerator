cc167fd18b25bb6c5daa40e9d5c6ade26394ea7b
Severin Brunner <severin.brunner@outlook.de>
2022-08-10 02:14:35 +0200

    Calculate expanded keys on-the-fly during encryption

--- AES/AES.srcs/sources_1/new/AEA.vhd
+++ AES/AES.srcs/sources_1/new/AEA.vhd
@@ -23,7 +23,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use work.common.ALL;
-
+use work.sbox_definition.sbox_encrypt;
 
 entity AEA is
     Port ( dIn : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
@@ -70,6 +70,44 @@ component KeyExpansion is
            Reset : in STD_LOGIC);
 end component;
 
+function mulGFby2(val : std_logic_vector(7 downto 0)) return std_logic_vector is
+    variable c : std_logic_vector(7 downto 0);
+begin
+    c := val(6 downto 0) & '0';
+    if val >= x"80" then
+        return c xor x"1b";
+    else
+        return c;
+    end if;
+end function;
+
+function calcNextKey(lastKey : std_logic_vector(127 downto 0); RCon : std_logic_vector(7 downto 0))
+return std_logic_vector is
+    variable key : std_logic_vector (127 downto 0);
+    variable word_1, word : std_logic_vector(31 downto 0);
+begin
+    -- Calculate first word
+    word_1 := lastKey(31 downto 0);
+    -- Rotate word: rol 8
+    word_1 := word_1(23 downto 0) & word_1(31 downto 24);
+    -- perform substitution for each byte of word_1
+    for j in 3 downto 0 loop
+        word_1((j+1)*8-1 downto j*8) := sbox_encrypt(to_integer(unsigned(word_1((j+1)*8-1 downto j*8))));
+    end loop;
+    -- xor with constant
+    word_1 := word_1 xor (Rcon & x"000000");  --TODO manually multiply Rcon by 2 before/after calling!
+    
+    word := word_1 xor lastKey(127 downto 96);
+    key(127 downto 96) := word;
+    
+    -- Calculate the other 3 words
+    for i in 1 to 3 loop
+        word := word xor lastKey(127-32*i downto 96-32*i);
+        key(127-32*i downto 96-32*i) := word;
+    end loop;
+    return key;
+end function;
+
 
 type state_type is (Idle, KeyExpState, RoundState, LastRoundState);
 
@@ -84,9 +122,12 @@ signal EnIPreARK, EnIRound : std_logic;
 signal EnOPreARK, EnORound : std_logic;
 signal isLastRound, roundIsLastCycle : std_logic;
 
--- signals for the KeyExpansion
+-- signals for the KeyExpansion, which is only used for decryption
 signal roundKeys : ROUNDKEYARRAY;
 signal EnIKeyExp, EnOKeyExp : std_logic;
+-- signals for calculating the key on-the-fly
+signal encryptKey : std_logic_vector(KEY_SIZE-1 downto 0);
+signal Rcon : std_logic_vector(7 downto 0);
 
 begin
 
@@ -95,6 +136,7 @@ preARK : AddRoundKey port map(dInPreARK, dOutPreARK, key, EnIPreARK, EnOPreARK, 
 roundAEA : AEA_Round port map(dInRound, dOutRound, roundKey, encrypt, isLastRound, EnIRound, EnORound, roundIsLastCycle, Clock, Reset);
 keyExp : KeyExpansion port map(key, roundKeys, EnIKeyExp, EnOKeyExp, Clock, Reset); 
 
+-- TODO KeyExpansion für encrypt='1' überspringen
 -- connect data signals of the components
 dInPreARK <= dIn when encrypt = '1' else dOutRound;
 dInRound <= dOutPreARK when encrypt = '1' and unsigned(currentRound) <= to_unsigned(1, 4) else
@@ -103,17 +145,21 @@ dInRound <= dOutPreARK when encrypt = '1' and unsigned(currentRound) <= to_unsig
 dOut <= dOutRound when encrypt = '1' else dOutPreARK;
 
 -- connect enable signals
-EnIKeyExp <= EnI; -- TODO muss das Enable-Signal einen Takt oder die ganze Zeit über gesendet werden?
-EnIPreARK <= EnOKeyExp when encrypt = '1' else EnORound;
+-- skip KeyExpansion for encryption
+EnIKeyExp <= '0' when encrypt = '1' else EnI; 
+EnIPreARK <= EnI when encrypt = '1' else EnORound;
 EnIRound <= EnOPreARK when encrypt = '1' and unsigned(currentRound) <= to_unsigned(1, 4) else
             EnOKeyExp when encrypt = '0' and unsigned(currentRound) = to_unsigned(10, 4) else -- decryption
-            EnORound; -- loop back output of round to the input
+            -- stop loopback in last round
+            '0' when state = Idle else
+             EnORound; -- loop back output of round to the input
 -- Set EnO to 0, except in the last round, where it is set to the enable signal of the last component
 EnO <= '0' when state /= Idle else 
         EnORound when encrypt = '1' else
         EnOPreARK; -- decryption
 
-roundKey <= roundKeys(to_integer(unsigned(currentRound)));
+roundKey <= encryptKey when encrypt = '1' else
+            roundKeys(to_integer(unsigned(currentRound)));
 
 process (Clock, Reset)
 begin
@@ -123,25 +169,30 @@ elsif rising_edge(Clock) then
     case(state) is
         when Idle =>
             if encrypt = '1' then
-                currentRound <= x"0";
+                currentRound <= x"1";
                 isLastRound <= '0';
+                -- start calculation on enable signal
+                if EnI = '1' then
+                    -- Calculate first key here, while preARK is running
+                    if encrypt = '1' then
+                        state <= RoundState;
+                        encryptKey <= calcNextKey(key, x"01"); -- Enter RCon manually the first time to avoid having to reset it
+                        RCon <= mulGFby2(x"01"); -- Set RCon to the next RCon
+                    end if;
+                end if;
             else
                 -- for decryption, start with the last round
                 currentRound <= x"a";
                 isLastRound <= '1';
-            end if;
-            -- start calculation on enable signal
-            if EnI = '1' then
-                state <= KeyExpState;
+                -- start calculation on enable signal
+                if EnI = '1' then
+                    state <= KeyExpState;
+                end if;
             end if;
         when KeyExpState =>
             -- Wait in this state until keys have been calculated
             if EnOKeyExp = '1' then
                 state <= RoundState;
-                -- in this round, the preARK is running, so the next round is round 1
-                if encrypt = '1' then
-                    currentRound <= x"1"; -- TODO kann ich gleich in Idle currentRound = 0 setzen?
-                end if;
             end if;
         when RoundState =>
             -- Increment currentRound and set the next roundKey in the cycle before the round finishes
@@ -163,12 +214,16 @@ elsif rising_edge(Clock) then
                     -- Next round is round 1, which is the last round, as round 0 is the preARK
                     state <= LastRoundState;
                 end if;
+                
+                -- Calculate key for next round
+                encryptKey <= calcNextKey(encryptKey, RCon);
+                RCon <= mulGFby2(RCon);
             end if;
         when LastRoundState =>
             -- wait until the last component gives its enable signal, then return to idle state
             if (encrypt = '1' and roundIsLastCycle = '1') or (encrypt = '0' and EnIPreARK = '1') then
                 state <= Idle;
-                currentRound <= x"0";
+                --currentRound <= x"0"; currentRound wird in Idle zurückgesetzt, das sollte reichen
                 IsLastRound <= '0';
             end if;
         when others =>
