1e836c68a8fbc850c37cdd1110af9a2495f46702
Severin Brunner <severin.brunner@outlook.de>
2022-08-16 02:12:53 +0200

    Restructure AES Core and control logic

--- AES/AES.srcs/sources_1/new/AES_Core.vhd
+++ AES/AES.srcs/sources_1/new/AES_Core.vhd
@@ -27,10 +27,6 @@ use work.common.ALL;
 -- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity AES_Core is
     Port ( Key : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
@@ -40,6 +36,8 @@ entity AES_Core is
            --SaveRestore : inout std_logic;
            EnI : in std_logic;
            EnO : out std_logic;
+           mode : in std_logic_vector (1 downto 0);
+           chaining_mode : in std_logic_vector (2 downto 0);
            Clock : in std_logic;
            Reset : in std_logic
            );
@@ -47,38 +45,19 @@ end AES_Core;
 
 architecture Behavioral of AES_Core is
 
-type state_type is (Idle, KeyExpState, RoundState, FinalRoundState);
-
 component KeyExpansion is
     Port ( userKey : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
            roundKeys : out ROUNDKEYARRAY;
-           Clock : in STD_LOGIC;
-           Reset : in STD_LOGIC);
-end component;
-
-component SubBytes is
-    Port ( dIn : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
-           dOut : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
-           encrypt : in STD_LOGIC;
            EnI : in STD_LOGIC;
            EnO : out STD_LOGIC;
            Clock : in STD_LOGIC;
            Reset : in STD_LOGIC);
 end component;
 
-component AddRoundKey is
-    Port ( din : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
-           dout : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
+component PipelinedAEA is
+    Port ( dIn : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
+           dOut : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
            key : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
-           EnI : in STD_LOGIC;
-           EnO : out STD_LOGIC;
-           Clock : in STD_LOGIC;
-           Reset : in STD_LOGIC);
-end component;
-
-component MixColumns is
-    Port ( dIn : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
-           dOut : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
            encrypt : in STD_LOGIC;
            EnI : in STD_LOGIC;
            EnO : out STD_LOGIC;
@@ -86,22 +65,74 @@ component MixColumns is
            Reset : in STD_LOGIC);
 end component;
 
-signal roundKeys : ROUNDKEYARRAY;
-signal currentRound : unsigned(4 downto 0);
+
+-- constant definitions
+constant MODE_ENCRYPTION : std_logic_vector := "00";
+constant MODE_KEYEXPANSION : std_logic_vector := "01";
+constant MODE_DECRYPTION : std_logic_vector := "10";
+constant MODE_KEYEXPANSION_AND_DECRYPT : std_logic_vector := "11";
+
+constant CHAINING_MODE_ECB : std_logic_vector := "000";
+constant CHAINING_MODE_CBC : std_logic_vector := "001";
+constant CHAINING_MODE_CTR : std_logic_vector := "010";
+constant CHAINING_MODE_GCM : std_logic_vector := "011";
+-- TODO GMAC?
+constant CHAINING_MODE_CCM : std_logic_vector := "100";
+
+-- signal definitions
+
+type state_type is (Idle, Busy);
+
 signal state : state_type;
 
+signal dInAEA, dOutAEA, keyAEA : std_logic_vector(KEY_SIZE-1 downto 0);
+signal encryptAEA, EnIAEA, EnOAEA : std_logic;
+
 begin
+
+-- TODO signal KeyExpansionOnly?
+algorithm : PipelinedAEA port map (dInaEA, dOutAEA, keyAEA, encryptAEA, EnIAEA, EnOAEA, Clock, Reset);
+
 
 process (Clock, Reset)
 begin
 if Reset = '0' then
     dout <= (others => '0');
     state <= Idle;
-    currentRound <= to_unsigned(0, 4);
+    EnIAEA <= '0';
+    EnO <= '0';
 elsif rising_edge(Clock) then
-    if EnI = '1' then
-    
-    end if;
+    case state is
+        when Idle =>
+            -- Start calculation on EnI = '1'
+            if EnI = '1' then
+                case mode is
+                    when MODE_ENCRYPTION =>
+                        encryptAEA <= '1';
+                        
+                    when MODE_KEYEXPANSION =>
+                        
+                    when MODE_DECRYPTION =>
+                        encryptAEA <= '0';
+                    when MODE_KEYEXPANSION_AND_DECRYPT =>
+                        encryptAEA <= '0';
+                end case;
+                case chaining_mode is
+                    when CHAINING_MODE_ECB =>
+                        -- No extra action needed
+                    when CHAINING_MODE_CBC =>
+                    
+                    when CHAINING_MODE_CTR =>
+                    
+                    when CHAINING_MODE_GCM =>
+                    
+                    when others =>
+                        -- error state? As other modes are not implemented
+                end case;
+            end if;
+        when Busy =>
+            -- TODO wait for AEA to finish, then set control bit
+    end case;
 end if;
 end process;
 
--- AES/AES.srcs/sources_1/new/ControlLogic.vhd
+++ AES/AES.srcs/sources_1/new/ControlLogic.vhd
@@ -40,21 +40,15 @@ entity ControlLogic is
 -- global signals
     RESETn   : in std_logic;
 -- Ports to the AHB interface
-    key0   : in std_logic_vector (31 downto 0);
-    key1   : in std_logic_vector (31 downto 0);
-    key2   : in std_logic_vector (31 downto 0);
-    key3   : in std_logic_vector (31 downto 0);
-    IV0   : in std_logic_vector (31 downto 0);
-    IV1   : in std_logic_vector (31 downto 0);
-    IV2   : in std_logic_vector (31 downto 0);
-    IV3   : in std_logic_vector (31 downto 0);
-    status : out std_logic_vector (31 downto 0);
-    control : in std_logic_vector (31 downto 0);
-    data_in : in std_logic_vector (31 downto 0);
-    data_out: out std_logic_vector (31 downto 0);
+    AES_KEYRx   : in std_logic_vector (KEY_SIZE-1 downto 0);
+    AES_IVRx    : in std_logic_vector (KEY_SIZE-1 downto 0);
+    AES_SR  : out std_logic_vector (31 downto 0);
+    AES_CR : in std_logic_vector (31 downto 0);
+    AES_DINR : in std_logic_vector (31 downto 0);
+    AES_DOUTR : out std_logic_vector (31 downto 0);
 -- Ports to the AES Core
     key : out std_logic_vector (KEY_SIZE-1 downto 0);
-    IVI : out std_logic_vector (KEY_SIZE-1 downto 0);
+    IV : out std_logic_vector (KEY_SIZE-1 downto 0);
     DIN : out std_logic_vector (KEY_SIZE-1 downto 0); -- TODO 32 oder 128 bit?
     DOUT : in std_logic_vector (KEY_SIZE-1 downto 0);
 -- Control to AES core
@@ -67,44 +61,31 @@ end ControlLogic;
 architecture Behavioral of ControlLogic is
 
 
-
-signal AES_KEYRx   : std_logic_vector (KEY_SIZE-1 downto 0);
-signal AES_IVRx    : std_logic_vector (KEY_SIZE-1 downto 0);
-signal AES_SR      : std_logic_vector (31 downto 0);
-signal AES_CR      : std_logic_vector (31 downto 0);
-signal AES_DINR    : std_logic_vector (31 downto 0);
-signal AES_DOUTR   : std_logic_vector (31 downto 0);
+-- TODO Was mit SUSPRx anfangen?
 signal AES_SUSPRx  : std_logic_vector (31 downto 0);
 
 begin
 
-AES_KEYRx <= key0 & key1 & key2 & key3;
-AES_IVRx <= IV0 & IV1 & IV2 & IV3;     
-
 status <= AES_SR;
 AES_CR <= control;
 
-AES_DINR <= data_in;
-data_out <= AES_DOUTR;
+DIN <= AES_DINR;
+AES_DOUTR <= DOUT;
 
 key <= AES_KEYRx;
-IVI <= AES_IVRx;
+IV <= AES_IVRx;
 
 -- set AES control signals
 en <= AES_CR(0);
 mode <= AES_CR(4 downto 3);
-chaining_mode <= AES_CR(6 downto 5);
+chaining_mode <= AES_CR(16) & AES_CR(6 downto 5);
 
 
+-- TODO Process to clear AES_SR
 process (RESETn)
 begin
 if RESETn = '0' then
-    for i in KEY_SIZE-1 downto 0 loop
-        AES_KEYRx(i) <= '0';
-        AES_IVRx(i) <= '0';
-    end loop;
     AES_SR <= x"00000000";
-    
 end if;
 end process;
 
