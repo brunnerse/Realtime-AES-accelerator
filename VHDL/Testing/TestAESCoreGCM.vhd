e50a83e9e381e3b0d2e6796457a8a9e69a3d1757
Severin Brunner <severin.brunner@outlook.de>
2022-08-21 15:42:41 +0200

    Test GCM Mode Register writing (successfully)

--- VHDL/Testing/TestAESCoreGCM.vhd
+++ VHDL/Testing/TestAESCoreGCM.vhd
@@ -2,6 +2,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.common.ALL;
+use work.addresses.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -15,13 +16,18 @@ end TestAESCoreGCM;
 architecture Behavioral of TestAESCoreGCM is
 
 component AES_Core is
+     generic (
+        ADDR_IV : integer;
+        ADDR_SUSP : integer;
+        ADDR_H  : integer
+        );
     Port ( Key : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
            IV : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
-           newIV : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
            H  : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
-           newH  : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
            Susp : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
-           newSusp : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
+           WrEn   : out STD_LOGIC;
+           WrAddr : out STD_LOGIC_VECTOR(ADDR_WIDTH-1 downto 0);
+           WrData : out STD_LOGIC_VECTOR(KEY_SIZE-1 downto 0);
            dIn : in STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
            dOut : out STD_LOGIC_VECTOR (KEY_SIZE-1 downto 0);
            EnI : in std_logic;
@@ -36,23 +42,25 @@ end component;
 
 signal Clock, Resetn : std_logic;
 
-signal testPlaintext, testIV, testKey, testCiphertext, newIV, testSusp, newSusp, H : std_logic_vector(KEY_SIZE-1 downto 0);
+signal testPlaintext, testIV, testKey, testCiphertext, testSusp, H : std_logic_vector(KEY_SIZE-1 downto 0);
 signal EnCoreI, EnCoreO : std_logic;
 
-signal mode, GCMPhase : std_logic_vector(1 downto 0);
-signal chaining_mode : std_logic_vector(2 downto 0);
+signal WrEn : std_logic;
+signal WrData : std_logic_vector(KEY_SIZE-1 downto 0);
+signal WrAddr  : std_logic_vector(ADDR_WIDTH-1 downto 0);
+
+signal mode, GCMPhase : std_logic_vector(1 downto 0) := MODE_ENCRYPTION;
+signal chaining_mode : std_logic_vector(2 downto 0) := CHAINING_MODE_GCM;
 
 begin
 
-
---testPlaintext <= x"00102030011121310212223203132333";
 testKey <= x"000102030405060708090a0b0c0d0e0f";
 
-mode <= MODE_KEYEXPANSION_AND_DECRYPTION;
-chaining_mode <= CHAINING_MODE_GCM;
 
+core: AES_Core 
+    generic map(ADDR_IV => ADDR_IVR0, ADDR_SUSP => ADDR_SUSPR0, ADDR_H => ADDR_SUSPR4)
+    port map (testKey, testIV, H, testSusp, WrEn, WrAddr, WrData, testPlaintext, testCiphertext, EnCoreI, EnCoreO, mode, chaining_mode, GCMPhase, Clock, Resetn);
 
-core: AES_Core port map (testKey, testIV, newIV, H, H, testSusp, newSusp, testPlaintext, testCiphertext, EnCoreI, EnCoreO, mode, chaining_mode, GCMPhase, Clock, Resetn);
 
 process begin
 Clock <= '1'; wait for 5 ns;
@@ -89,21 +97,18 @@ wait until EnCoreO <= '1'; wait for 10ns;
 -- Enter payload phase
 GCMPhase <= GCM_PHASE_PAYLOAD;
 EnCoreI <= '1';
-testIV <= x"f0e0d0c0b0a090807060504000000002";
 testPlaintext <= x"00102030011121310212223203132333";
 wait for 10ns;
 EnCoreI <= '0';
 wait until EnCoreO <= '1'; wait for 10ns;
 wait for 10ns;
 -- second payload block
-testIV <= newIV;
 EnCoreI <= '1';
 wait for 10ns;
 EnCoreI <= '0';
 wait until EnCoreO <= '1'; wait for 10ns;
 -- third payload block
 testPlaintext <= testKey;
-testIV <= newIV;
 EnCoreI <= '1';
 wait for 10ns;
 EnCoreI <= '0';
@@ -112,7 +117,7 @@ wait until EnCoreO <= '1'; wait for 10ns;
 GCMPhase <= GCM_PHASE_FINAL;
 -- gLen(header) & gLen(cipher), length in bits
 testPlaintext <= x"0000000000000100" & x"0000000000000180";
-testIV <= x"f0e0d0c0b0a090807060504000000001";
+wait for 10ns;
 --testPlaintext <= (others => '0');
 EnCoreI <= '1';
 wait for 10ns;
@@ -121,15 +126,26 @@ wait;
 end process;
 
 -- process to update the IV
-process (EnCoreO, Resetn) 
+process (Resetn, Clock, WrEn, GCMPhase) 
 begin
 -- initialize IV
 if Resetn = '0' then
     testSusp <= (others => '0');
- end if;
+    testIV <= x"f0e0d0c0b0a090807060504000000002";
 -- update IV
-if EnCoreO = '1' and GCMPhase /= GCM_PHASE_INIT then
-    testSusp <= newSusp;
+elsif rising_edge(Clock) then
+    if WrEn = '1' then
+        if WrAddr = std_logic_vector(to_unsigned(ADDR_IVR0, ADDR_WIDTH)) then
+            testIV <= WrData;
+        elsif WrAddr = std_logic_vector(to_unsigned(ADDR_SUSPR0, ADDR_WIDTH)) then
+            testSusp <= WrData;
+        elsif WrAddr = std_logic_vector(to_unsigned(ADDR_SUSPR4, ADDR_WIDTH)) then
+            H <= WrData;   
+        end if;
+     end if;
+     if GCMPhase = GCM_PHASE_FINAL then
+        testIV <=  x"f0e0d0c0b0a090807060504000000001";
+     end if;
 end if;
 end process;
 
