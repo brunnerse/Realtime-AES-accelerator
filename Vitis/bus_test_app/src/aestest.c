d30335b5f636ef2c9249662a4909141953325ae6
Severin Brunner <severin.brunner@outlook.de>
2022-09-28 23:57:46 +0200

    Test different AES modi in aestest.c

--- Vitis/bus_test_app/src/aestest.c
+++ Vitis/bus_test_app/src/aestest.c
@@ -3,8 +3,10 @@
 #include "xuartps.h"
 #include "xgpiops.h"
 
+#include "xil_printf.h"
 #include <stdio.h>
 #include <string.h>
+
 
 #include "AES_Interface.h"
 
@@ -14,16 +16,30 @@ void hexToString(u8 *array, int len, char* outStr)
             sprintf(outStr+i*3, "%02x ", array[i]);
 }
 
+void hexToStdOut(u8* array, int len)
+{
+	xil_printf("\t");
+    for (int i = 0; i < len; i++)
+    {
+            xil_printf("%02x ", array[i]);
+            if ((i+1) % BLOCK_SIZE == 0)
+            	xil_printf("\n\r\t");
+    }
+    xil_printf("\n\r");
+}
+
+
 #define AES_BASEADDR XPAR_AES_INTERFACE_0_S_AXI_BASEADDR
 #define DDR_BASEADDR XPAR_PS7_DDR_0_S_AXI_BASEADDR
 
 int main()
 {
-    print("Starting main\n\r");
+    xil_printf("Starting main\n");
 	// Do a test write to the DDR memory
-    u32 val = Xil_In32(DDR_BASEADDR);
+    /*u32 val = Xil_In32(DDR_BASEADDR);
 	Xil_Out32(DDR_BASEADDR, 0xdeadbeef);
 	val = Xil_In32(DDR_BASEADDR);
+	*/
 
     int status;
 
@@ -40,27 +56,31 @@ int main()
     //u8 key[BLOCK_SIZE] =  {0x03, 0x02, 0x01, 0x00, 0x07, 0x06, 0x05, 0x04, 0x0b, 0x0a, 0x09, 0x08, 0x0f, 0x0e, 0x0d, 0x0c };
 
     u8 readKey[BLOCK_SIZE];
-    AES_GetKey(&aes, readKey);
     AES_SetKey(&aes, key);
     AES_GetKey(&aes, readKey);
 
     //AES_PerformKeyExpansion(&aes);
 
+    /*
     Mode mode = MODE_ENCRYPTION;
     AES_SetMode(&aes, mode);
-    AES_SetChainingMode(&aes, CHAINING_MODE_ECB);
+
     ChainingMode chMode = AES_GetChainingMode(&aes);
+    */
 
     u8 ciphertext[BLOCK_SIZE];
 
     AES_SetEnabled(&aes, 1);
+    AES_SetChainingMode(&aes, CHAINING_MODE_ECB);
     AES_processBlock(&aes, plaintext, ciphertext);
     AES_SetEnabled(&aes, 0);
 
     char debugText[500];
-
+    xil_printf("===== ECB ======\n\r");
     hexToString(plaintext, 16, debugText);
+    xil_printf("Plaintext:\n\r\t%s\n\r", debugText);
     hexToString(ciphertext, 16, debugText);
+    xil_printf("Ciphertext:\n\r\t%s\n\r", debugText);
 
     memcpy(plaintext, ciphertext, 16);
 
@@ -70,17 +90,57 @@ int main()
 	AES_SetEnabled(&aes, 0);
 
 
+	xil_printf("Ciphertext after decryption:\n\r");
+	hexToStdOut(ciphertext, 16);
 
-    XUartPs_Config *uartConfig = XUartPs_LookupConfig(XPAR_PS7_UART_1_DEVICE_ID);
-    XUartPs uart;
-    status =  XUartPs_CfgInitialize(&uart, uartConfig, uartConfig->BaseAddress);
 
-    status = XUartPs_SelfTest(&uart);
-    //XUartPs_SetBaudRate(&uart, 9600);
-    XUartPsFormat format;
-    XUartPs_GetDataFormat(&uart, &format);
-    u8 str[8] = {49, 50, 51, 52, 53, 54, 10, 13};
-    XUartPs_Send(&uart, str, 8);
+	// Test the different modi
+	u8 block1[16] = {0x00, 0x10, 0x20, 0x30, 0x01, 0x11, 0x21, 0x31, 0x02, 0x12, 0x22, 0x32, 0x03, 0x13, 0x23, 0x33};
+	u8 block2[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+	u8 block3[16] = {0xaf, 0xfe, 0xde, 0xad, 0xbe, 0xef, 0xda, 0xdc, 0xab, 0xbe, 0xad, 0xbe, 0xec, 0x0c, 0xab, 0xad};
+
+	u8 IV[16] = {0xf0, 0xe0, 0xd0, 0xc0, 0xb0, 0xa0, 0x90, 0x80, 0x70, 0x60, 0x50, 0x40, 0x30, 0x20, 0x10, 0x00};
+
+	u8 data[16*3];
+	memcpy(data, block1, 16);
+	memcpy(data+16, block2, 16);
+	memcpy(data+16*2, block3, 16);
+
+	u8 cipherdata[16*3];
+	u8 deciphered_cipherdata[16*3];
+
+
+	xil_printf("======================= Testing the different modi ====================\n\r");
+	for (int i = 0; i < 3; i++)
+	{
+		if (i == 0)
+		{
+			xil_printf("\n==== ECB ====\n\r");
+			AES_processDataECB(&aes, 1, data, cipherdata, 16*3);
+			AES_processDataECB(&aes, 0, cipherdata, deciphered_cipherdata, 16*3);
+		}
+		else if (i == 1)
+		{
+			xil_printf("\n==== CBC ====\n\r");
+			AES_processDataCBC(&aes, 1, data, cipherdata, 16*3, IV);
+			AES_processDataCBC(&aes, 0, cipherdata, deciphered_cipherdata, 16*3, IV);
+		}
+		else
+		{
+			xil_printf("\n==== CTR ====\n\r");
+			AES_processDataCTR(&aes, data, cipherdata, 16*3, IV);
+			AES_processDataCTR(&aes, cipherdata, deciphered_cipherdata, 16*3, IV);
+		}
+		xil_printf("Plaintext:\n\r");
+		hexToStdOut(data, 16*3);
+		xil_printf("Ciphertext:\n\r");
+		hexToStdOut(cipherdata, 16*3);
+		xil_printf("Deciphered Ciphertext:\n\r");
+		hexToStdOut(deciphered_cipherdata, 16*3);
+	}
+
+
+
 
     // Configure LED as output
     XGpioPs_Config *GPIO_Config = XGpioPs_LookupConfig(XPAR_PS7_GPIO_0_DEVICE_ID);
