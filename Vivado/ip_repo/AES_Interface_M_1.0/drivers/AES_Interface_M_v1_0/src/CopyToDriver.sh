4992509a4e74a4f82b0a79281fab6a81ea1ceaa2
Severin Brunner <severin.brunner@outlook.de>
2022-10-13 00:10:05 +0200

    Add shell script to quickly replace all driver files in the Vitis Project

---
+++ Vivado/ip_repo/AES_Interface_M_1.0/drivers/AES_Interface_M_v1_0/src/CopyToDriver.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+path=~/Git_Repositories/_MasterCode/Vitis/aes_zedboard_platform/
+
+for file in $(ls)
+do
+if [[ $file == Makefile || ./$file == $0 ]]
+then
+	echo "Not copying the makefile and itself!"
+else
+	results=$(find $path -name $file) 
+	for r in $results
+	do
+		echo Copying file $file to $r ...
+		cp $file $r	
+	done
+fi
+done
